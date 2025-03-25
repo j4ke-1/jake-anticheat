5773d90215449e76b603682222ac25ddce77df45
Jake <JakeHagele@gmail.com>
2025-03-25 10:10:02 -0700

    Update client.lua

--- jake-anticheat/client/client.lua
+++ jake-anticheat/client/client.lua
@@ -1,9 +1,16 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
--- Check inventory every 30 seconds
+-- Check inventory and weapon in hand every 30 seconds
 Citizen.CreateThread(function()
     while true do
-        Citizen.Wait(30000) -- 30 seconds
-        TriggerServerEvent('qb-anticheat:server:checkInventory')
+        Citizen.Wait(30000) -- Check every 30 seconds
+        local ped = PlayerPedId()
+        local weaponHash = GetSelectedPedWeapon(ped)
+
+        -- Trigger server-side checks
+        TriggerServerEvent('qb-anticheat:server:checkInventory') -- Check blacklisted items
+        if weaponHash ~= GetHashKey("WEAPON_UNARMED") then -- Ignore unarmed state
+            TriggerServerEvent('qb-anticheat:server:checkWeaponInHand', weaponHash) -- Check weapon in hand
+        end
     end
 end)
